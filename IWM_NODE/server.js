e0a9dddea43237596ff98540a1e998e4397ac82b
LucasLaurens <lucaslaurens.74@gmail.com>
2020-05-27 16:39:18 +0200

    clean up

--- IWM_NODE/server.js
+++ IWM_NODE/server.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const port = 8080
-// const datas = require('./datas.json')
-// const tennisplayers = require('./tennisplayers.json')
 
 let app = express()
 let bodyParser = require('body-parser')
@@ -41,10 +39,6 @@ app.post('/', (req, res) => {
     if (req.body.message === undefined || req.body.message === '') {
         req.flash('error', "Vous n'avez pas écrit de message")
         res.redirect('/')
-
-        // res.render('pages/base', {
-        //     error: 'Vous n\'avez pas entré de message'
-        // })
     } else {
         console.log(req.body.message)
         try {
@@ -72,70 +66,5 @@ app.get('/message/:id', function (req, res) {
         console.error(e)
     }
 })
-
-
-// Manage users
-// app.get('/users', (req, res) => {
-//     res.json(datas)
-// })
-
-// app.post('/new-user', (req, res) => {
-//     let new_data = {
-//         firstname: 'Lucas',
-//         lastname: 'Laurens',
-//         old: 24
-//     }
-
-//     datas.students.push(new_data)
-
-//     res.redirect('/users')
-// })
-
-// Json tennis players
-    // app.get('/players', (req, res) => {
-    //     res.json(tennisplayers)
-    // })
-
-// Json tennis players with last name param in url
-// app.get('/players/find/:lastname', (req, res) => {
-//     const { lastname } = req.params
-//     let result = tennisplayers.players.filter(player => player.lastname.toUpperCase() === lastname.toUpperCase())
-
-//     result && result !== undefined ? res.json(result) : res.sendStatus(404)
-// })
-
-// Json tennis players with id param in url
-// app.get('/players/:id', (req, res) => {
-//     const { id } = req.params
-//     let result = tennisplayers.players.filter(player => player.id == id)
-
-//     result && result !== undefined ? res.json(result) : res.sendStatus(404)
-// })
-
-// Json students
-// app.get('/students', (req, res) => {
-//     res.json(datas)
-// })
-
-// Json students
-// app.get('/students/:lastname', (req, res) => {
-//     const { lastname } = req.params
-//     let result = datas.students.filter(student => student.lastname.toUpperCase() === lastname.toUpperCase())
-
-//     result && result !== undefined ? res.json(result) : res.sendStatus(404)
-
-// })
-
-// Json test
-// app.get('/home', (req, res) => {
-//     res.send('Bienvenue')
-// })
-
-// Json test
-// app.get('/tableau', (req, res) => {
-//     const arr = [1, 2, 3]
-//     res.send(arr)
-// })
-
 
 app.listen(port)
