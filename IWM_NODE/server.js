12c279d1b9460273f53cec51b2dac18ff3646e60
LucasLaurens <lucaslaurens.74@gmail.com>
2020-05-27 12:08:17 +0200

    exercice tennis players

--- IWM_NODE/server.js
+++ IWM_NODE/server.js
@@ -1,14 +1,47 @@
 let app = require('express')()
 const datas = require('./datas.json')
+const tennisplayers = require('./tennisplayers.json')
 const port = 8080
 
 
 app.set('view engine', 'ejs')
 
+// Home with template
+app.get('/', (req, res) => {
+    res.render('pages/index', {
+        test: 'salut'
+    })
+})
+
+// Json tennis players
+app.get('/players', (req, res) => {
+    res.json(tennisplayers)
+})
+
+// Json tennis players with last name param in url
+app.get('/players/find/:lastname', (req, res) => {
+    const { lastname } = req.params
+    let result = tennisplayers.players.filter(player => player.lastname.toUpperCase() === lastname.toUpperCase())
+
+    result && result !== undefined ? res.json(result) : res.sendStatus(404)
+})
+
+// Json tennis players with id param in url
+app.get('/players/:id', (req, res) => {
+    const { id } = req.params
+    let result = tennisplayers.players.filter(player => player.id == id)
+
+    result && result !== undefined ? res.json(result) : res.sendStatus(404)
+})
+
+
+
+// Json students
 app.get('/students', (req, res) => {
     res.json(datas)
 })
 
+// Json students
 app.get('/students/:lastname', (req, res) => {
     const { lastname } = req.params
     let result = datas.students.filter(student => student.lastname.toUpperCase() === lastname.toUpperCase())
@@ -17,19 +50,16 @@ app.get('/students/:lastname', (req, res) => {
 
 })
 
+// Json test
 app.get('/home', (req, res) => {
     res.send('Bienvenue')
 })
 
+// Json test
 app.get('/tableau', (req, res) => {
     const arr = [1, 2, 3]
     res.send(arr)
 })
 
-app.get('/', (req, res) => {
-    res.render('pages/index', {
-        test: 'salut'
-    })
-})
 
 app.listen(port)
