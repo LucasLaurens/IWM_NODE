4fa9418ddffb75e0924c52949aa1fd4669ef62b9
LucasLaurens <lucaslaurens.74@gmail.com>
2020-05-27 14:32:32 +0200

    [FEAT] send message and get error

--- IWM_NODE/server.js
+++ IWM_NODE/server.js
@@ -1,69 +1,98 @@
 const express = require('express')
-const app = express()
+let app = express()
+let bodyParser = require('body-parser')
+let session = require('express-session')
 
-const datas = require('./datas.json')
-const tennisplayers = require('./tennisplayers.json')
+
+// const datas = require('./datas.json')
+// const tennisplayers = require('./tennisplayers.json')
 
 const port = 8080
 
 // set templating
 app.set('view engine', 'ejs')
+app.set('trust proxy', 1)
 
-// set distribute files
+// set distribute files (middleware)
 app.use('/assets', express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(session({
+    secret: 'aze',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}))
+
+// Routes
 
 // Home with template
 app.get('/', (req, res) => {
-    res.render('pages/base', {
-        test: 'salut'
-    })
+    console.log(req.session.error)
+    if (req.session.error) {
+        res.locals.error = req.session.error
+        console.log(res.locals.error)
+        req.session.error = undefined
+    }
+    res.render('pages/base')
+})
+
+// Post message in Home
+app.post('/', (req, res) => {
+    if(req.body.message === undefined || req.body.message === '') {
+        req.session.error = "Vous n'avez pas entré de message"
+        res.redirect('/')
+        // res.render('pages/base', {
+        //     error: 'Vous n\'avez pas entré de message'
+        // })
+    }
 })
 
 // Json tennis players
-app.get('/players', (req, res) => {
-    res.json(tennisplayers)
-})
+    // app.get('/players', (req, res) => {
+    //     res.json(tennisplayers)
+    // })
 
 // Json tennis players with last name param in url
-app.get('/players/find/:lastname', (req, res) => {
-    const { lastname } = req.params
-    let result = tennisplayers.players.filter(player => player.lastname.toUpperCase() === lastname.toUpperCase())
+// app.get('/players/find/:lastname', (req, res) => {
+//     const { lastname } = req.params
+//     let result = tennisplayers.players.filter(player => player.lastname.toUpperCase() === lastname.toUpperCase())
 
-    result && result !== undefined ? res.json(result) : res.sendStatus(404)
-})
+//     result && result !== undefined ? res.json(result) : res.sendStatus(404)
+// })
 
 // Json tennis players with id param in url
-app.get('/players/:id', (req, res) => {
-    const { id } = req.params
-    let result = tennisplayers.players.filter(player => player.id == id)
+// app.get('/players/:id', (req, res) => {
+//     const { id } = req.params
+//     let result = tennisplayers.players.filter(player => player.id == id)
 
-    result && result !== undefined ? res.json(result) : res.sendStatus(404)
-})
-
-// Json students
-app.get('/students', (req, res) => {
-    res.json(datas)
-})
+//     result && result !== undefined ? res.json(result) : res.sendStatus(404)
+// })
 
 // Json students
-app.get('/students/:lastname', (req, res) => {
-    const { lastname } = req.params
-    let result = datas.students.filter(student => student.lastname.toUpperCase() === lastname.toUpperCase())
+// app.get('/students', (req, res) => {
+//     res.json(datas)
+// })
 
-    result && result !== undefined ? res.json(result) : res.sendStatus(404)
+// Json students
+// app.get('/students/:lastname', (req, res) => {
+//     const { lastname } = req.params
+//     let result = datas.students.filter(student => student.lastname.toUpperCase() === lastname.toUpperCase())
 
-})
+//     result && result !== undefined ? res.json(result) : res.sendStatus(404)
+
+// })
 
 // Json test
-app.get('/home', (req, res) => {
-    res.send('Bienvenue')
-})
+// app.get('/home', (req, res) => {
+//     res.send('Bienvenue')
+// })
 
 // Json test
-app.get('/tableau', (req, res) => {
-    const arr = [1, 2, 3]
-    res.send(arr)
-})
+// app.get('/tableau', (req, res) => {
+//     const arr = [1, 2, 3]
+//     res.send(arr)
+// })
 
 
 app.listen(port)
